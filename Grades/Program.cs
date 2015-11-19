5ecedf9c7c83197ef016294395acdc8de4d47f2b
Sanjay Purswani <sanjsanj@hotmail.com>
2015-11-19 11:09:26 +0000

    first commit

---
+++ Grades/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Speech.Synthesis;
+
+namespace Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SpeechSynthesizer synth = new SpeechSynthesizer();
+            synth.Speak("Hello! This is the grade book program");
+
+            GradeBook book = new GradeBook();
+            book.AddGrade(91);
+            book.AddGrade(89.5f);
+            book.AddGrade(75);
+
+            GradeStatistics stats = book.ComputeStatistics();
+            System.Console.WriteLine(stats.AverageGrade);
+            System.Console.WriteLine(stats.HighestGrade);
+            System.Console.WriteLine(stats.LowestGrade);
+        }
+    }
+}
