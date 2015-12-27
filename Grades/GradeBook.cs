5411262f7f9a33c84d9621746c98094350687ef4
Sanjay Purswani <sanjsanj@hotmail.com>
2015-12-27 16:03:05 +0000

    Add namechanged check in name setter so it does not throw a null exception error

--- Grades/GradeBook.cs
+++ Grades/GradeBook.cs
@@ -55,11 +55,13 @@ namespace Grades
                     throw new ArgumentException("Name cannot be null or empty");
                 }
 
-                if (_name != value)
+                if (_name != value && NameChanged != null)
                 {
                     NameChangedEventArgs args = new NameChangedEventArgs();
                     args.ExistingName = _name;
                     args.NewName = value;
+
+                    NameChanged(this, args);
                 }
                 _name = value;
             }
